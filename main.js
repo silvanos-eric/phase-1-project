8a60f5d4282bd40cae0d18d00145b9f7d5ff6e3e
Silvanos Eric <ericoderito@gmail.com>
2024-07-19 08:59:53 +0300

    feat: add functionality to fetch and display quotes
    - Add an image to the quote card in index.html.
    - Create main.js to fetch a quote from an API and display it.
    
    index.html:
    - Add an image element within the quote card.
    
    main.js:
    - Select elements of interest from the DOM.
    - Implement `main` function to fetch and display a quote.
    - Add `getQuote` function to fetch a quote from an API.
    - Add `displayQuote` function to update the DOM with fetched quote details.

---
+++ main.js
@@ -0,0 +1,39 @@
+// Elements of interest
+const quoteCard = document.querySelector(".card");
+const quoteNumber = document.querySelector(".card-title");
+const quoteAdvice = document.querySelector(".card-text");
+const getNewQuoteBtn = document.querySelector(".card button");
+
+const main = async () => {
+  // Get a sample quote from API
+  const quoteData = await getQuote();
+
+  // Display quote
+  displayQuote(quoteData);
+};
+
+main();
+
+// Utility functions
+async function getQuote() {
+  try {
+    const response = await fetch("https://api.adviceslip.com/advice");
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const quoteData = await response.json();
+    return quoteData.slip;
+  } catch (error) {
+    console.error(
+      `There was a problem with your fetch operation: ${error.message}`
+    );
+  }
+}
+
+function displayQuote(quoteData) {
+  console.log(quoteData);
+  quoteNumber.textContent = `Advice # ${quoteData.id}`;
+  quoteAdvice.textContent = quoteData.advice;
+}
